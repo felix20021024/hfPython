8e34897d64a16fa292c07c4389d62c6f26baa47e
yaolei3 <yaolei3@asiainfo.com>
2017-08-20 21:12:47 +0800

    add list

--- src/chap01/Example01.py
+++ src/chap01/Example01.py
@@ -1,3 +1,7 @@
 __author__ = 'Administrator'
 
-print("hello");
+print("hello")
+movies=["The Holy Grail","The Life of Brain","The Meaning of Life","War Star"];
+print(movies)
+print(movies[1])
+print(movies[2])
